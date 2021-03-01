54c378e42a3c769b5c91537253212b1bf73c0ac1
EC2 Default User <ec2-user@ip-172-31-17-183.ec2.internal>
2021-03-01 08:15:04 +0000

    code style fixed, functionallity added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import './App.css';
 import './Board.css';
 import { Board } from './Board.js';
 import { ListItem } from './ListItem.js';
-//import { Login } from './Login.js';
 import { useState,useRef, useEffect } from 'react';
 import io from 'socket.io-client';
 import React from "react"
 
-
-const socket = io(); //socket connection
-
+const socket = io(); 
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
@@ -17,20 +14,21 @@ function App() {
   const inputRef = useRef(null);
   const [user_name, setUser_name] = useState([]);
   const [currentView, setCurrentView] = React.useState("view1");
+  const winner = calculateWinner(board);
+  const [ isXNext, setIsXNext ] = useState(true);
+  const nextSymbol = isXNext ? "O" : "X";
   
   
   const ViewOne = ({onClick}) => (
-  <div>
-    View 1 <br />
-     Enter your user name here: <input ref={inputRef} type="text" />
-      {/*<button onClick={onClickButton}></button>*/}
+  <div class="buttons">
+     Enter your user name: <input ref={inputRef} type="text" />
       <button onClick={onClickButton}>send</button>
       <ul>
         {user_name.map((item, index) => <ListItem key={index} name={item} />)}
       </ul>
    <button onClick={() => onClick("view2")}>Login to Game</button>
   </div>
-);
+  );
 
 
   function  onClickButton() {
@@ -40,11 +38,57 @@ function App() {
       socket.emit('login', { message: user_name });
     }
   }
+  
+   function getStatus() {
+    if (winner) {
+      return ( <h4> Winner:  {winner} </h4>);
+    } else if (isBoardFull(board)) {
+      return "Draw!";
+    } else {
+      return "Next player: " + nextSymbol;
+    }
+  }
+  
+  
+  function isBoardFull(board) {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] == null) {
+      return false;
+    }
+  }
+  return true;
+ }
+
+	
+  function refreshPage() {
+    window.location.reload(false);
+  }
+  
+  
+  function renderRestartButton() {
+    return (
+      <Restart
+        onClick={() => {
+          setBoard(Array(9).fill(null));
+          setIsXNext(true);
+        }}
+      />
+    );
+  }
+  
+  
+  function Restart({ onClick }) {
+  return (
+    <button className="restart" onClick={onClick}>
+      Play again
+    </button>
+  );
+  }
 
 
   const ViewTwo = ({onClick}) => (
-  
    <div>
+   <h2 class="header">Welcome to TIC TOC TOE Game</h2>
    <ul>
       <div class="board">
         <div class="box" id="box1" onClick={() => onClickT(0)}>{board[0]}</div>
@@ -57,11 +101,14 @@ function App() {
         <div class="box" id="box8" onClick={() => onClickT(7)}>{board[7]}</div>
         <div class="box" id="box9" onClick={() => onClickT(8)}>{board[8]}</div>
         </div>
-        {board.map(item => <Board onClick={item} /> )}
+        <div className="game-info">{getStatus()}</div>
+        <div className="restart-button">{renderRestartButton()}</div>
+        <div className="game-info">
+        <button onClick={refreshPage}>Click to reload!</button>
+        </div>
     </ul>
     {calculateWinner(board,user_name)};
     </div>
-
     );
     
     
@@ -79,7 +126,9 @@ function App() {
         for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (boardCopy[a] && boardCopy[a] === boardCopy[b] && boardCopy[a] === boardCopy[c]) {
-            return 'Winner is: ' + name + ' ' + boardCopy[a]  ;
+            return (
+              'Winner is: '+ boardCopy[a] 
+              );
         }
       }
     return null;
@@ -88,18 +137,10 @@ function App() {
    
     const onClickT = i => {
     const boardCopy = [...board];
-    if (state2==0){
-      boardCopy[i]= 'O' ;
+      boardCopy[i]= nextSymbol ;
       setBoard(boardCopy);
-      setState2(1);
-    }
-    else{
-      boardCopy[i]='X';
-      setBoard(boardCopy);
-      setState2(0);
-    }
-    //boardCopy[i]='X';
-    //setBoard(boardCopy);
+      setIsXNext(!isXNext);
+
     socket.emit('chat', { boardCopy: i } );
   };
  
@@ -108,17 +149,9 @@ function App() {
   useEffect(() => {
     socket.on('chat', (data) => {
       const boardCopy = [...board];
-      if (state2==0){
-         boardCopy[data.boardCopy]='O';
+         boardCopy[data.boardCopy]=nextSymbol;
          setBoard(boardCopy);
-         setState2(1);
-        
-    }
-    else{
-       boardCopy[data.boardCopy]='X';
-       setBoard(boardCopy);
-       setState2(0);
-    }
+         setIsXNext(!isXNext);
     });
    
     
@@ -126,97 +159,28 @@ function App() {
       const user_nameCopy = [...user_name];
       console.log(name);
       if (state2==0){
-        user_nameCopy[name.user_nameCopy]='O';
+        user_nameCopy[name.user_nameCopy]=nextSymbol;
         console.log(user_nameCopy[name.user_nameCopy]);
         calculateWinner(board,name);
       }
-      else{
-        user_nameCopy[name.user_nameCopy]='X';
+      else {
+        user_nameCopy[name.user_nameCopy]=nextSymbol;
         console.log(user_nameCopy[name.user_nameCopy]);
         calculateWinner(board,name);
       }
     });
         
-      
-    
     return ()=> {socket.off()};
   },[board,setState2,user_name]);
   
- 
- 
-  
-  
   return (
-  
     <div>
-    
           {
           currentView === "view1" ? 
           <ViewOne onClick={page => setCurrentView(page)} /> : 
           <ViewTwo onClick={page => setCurrentView(page)} />
        }
-    
-    
-     {/*
-    <h1> Tic Toc Toe </h1>
-    
-       Login here: <input ref={inputRef} type="text" />
-      <button onClick={onClickButton}>Send</button>
-      <ul>
-        {user_name.map((item, index) => <ListItem key={index} name={item} />)}
-      </ul>
-   
-
-     
-     <ul>
-      <div class="board">
-        <div class="box" id="box1" onClick={() => onClick(0)}>{board[0]}</div>
-        <div class="box" id="box2" onClick={() => onClick(1)}>{board[1]}</div>
-        <div class="box" id="box3" onClick={() => onClick(2)}>{board[2]}</div>
-        <div class="box" id="box4" onClick={() => onClick(3)}>{board[3]}</div>
-        <div class="box" id="box5" onClick={() => onClick(4)}>{board[4]}</div>
-        <div class="box" id="box6" onClick={() => onClick(5)}>{board[5]}</div>
-        <div class="box" id="box7" onClick={() => onClick(6)}>{board[6]}</div>
-        <div class="box" id="box8" onClick={() => onClick(7)}>{board[7]}</div>
-        <div class="box" id="box9" onClick={() => onClick(8)}>{board[8]}</div>
-        </div>
-        {board.map(item => <Board onClick={item} /> )}
-    </ul>
-*/}
     </div>
   );
 }
 export default App;
-
-
-{/*
-import logo from './logo.svg';
-import './App.css';
-import { Listitem } from './Listitem.js'
-import { useState, useRef } from 'react';
-//import { useState, useRef } from 'react'
-
-function App() {
-  const [myList,changeList]=useState([])
-  const inputRef=useRef(null);
-  
-  function onClickButton(){
-    const userText=inputRef.current.value;
-    changeList(prevList => [...prevList,userText]);
-  }
-  
-  
-  return (
-    <div>
-      <h1> Tic Toc Toe </h1>
-      <input ref={inputRef} type="text" />
-      <button onClick={onClickButton}>Add to list</button>
-      <ul>
-      {myList.map(item => <Listitem name={item} />)}
-      </ul>
-    </div>
-  );
-}
-
-export default App;
-*/}
