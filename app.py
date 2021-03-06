646a9890abfdcaffba096f816518a96ffad24064
EC2 Default User <ec2-user@ip-172-31-17-183.ec2.internal>
2021-03-06 05:11:22 +0000

    database setup

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 import os
-from flask import Flask, send_from_directory, json, session
+from flask import Flask, send_from_directory, json
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 app = Flask(__name__, static_folder='./build/static')
+
+# Point SQLAlchemy to your Heroku database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# Gets rid of a warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# IMPORTANT: This must be AFTER creating db variable to prevent
+# circular import issues
+import models
+db.create_all()
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -27,16 +42,35 @@ def index(filename):
 def on_connect():
     print('User connected!')
 
+
+    
+@socketio.on('user_list')
+def on_userList():
+    print('User connected!')
+    #winner = data['users']
+    rank = 100
+    all_people = models.Person.query.all()
+    users = []
+    ranks = []
+
+    for person in all_people:
+        users.append(person.username)
+        ranks.append(person.rank)
+   
+        
+            
+    print(users)
+    print(ranks)
+    socketio.emit('user_list', {'users': users})
+    
+     
+
 # When a client disconnects from this Socket connection, this function is run
 @socketio.on('disconnect')
 def on_disconnect():
     print('User disconnected!')
 
-@socketio.on('winner')
-def on_winner(data):
-    print(data)
-    winner_person = data['message']
-    socketio.emit('winner', winner_person, broadcast=True, include_self=False)
+
 
 @socketio.on('login')
 def on_login(data):
@@ -44,7 +78,7 @@ def on_login(data):
     name = data['message']
     Login_name.append(name)
     print(Login_name)
-    socketio.emit('login', Login_name, broadcast=True, include_self=False)
+    socketio.emit('login', Login_name, broadcast=True, include_self=True)
     
 # When a client emits the event 'chat' to the server, this function is run
 # 'chat' is a custom event name that we just decided
@@ -55,9 +89,23 @@ def on_chat(data): # data is whatever arg you pass in your emit call on client
     # the client that emmitted the event that triggered this function
     socketio.emit('chat',  data, broadcast=True, include_self=False)
 
+@socketio.on('join')
+def on_join(data): # data is whatever arg you pass in your emit call on client
+    print(str(data))
+    new_user = models.Person(username=data['user'], email='{0}@stuff.com'.format(data['user']))
+    db.session.add(new_user)
+    db.session.commit()
+    all_people = models.Person.query.all()
+    users = []
+    for person in all_people:
+        users.append(person.username)
+    socketio.emit('user_list', {'users': users})
+
+# Note we need to add this line so we can import app in the python shell
+if __name__ == "__main__":
 # Note that we don't call app.run anymore. We call socketio.run with app arg
-socketio.run(
-    app,
-    host=os.getenv('IP', '0.0.0.0'),
-    port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-)
+    socketio.run(
+        app,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
+    )
